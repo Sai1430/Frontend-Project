2d7102bf4cee5511b3fa2e95c4750c21123f4f3a
Sai Krishna Vamsi <saikrishnavamsi7@gmail.com>
2023-10-06 10:12:06 +0530

    Update Java Script.js

--- Java Script.js
+++ Java Script.js
@@ -1,23 +1,39 @@
-// Function to handle sending a message
-function sendMessage() {
-    const messageInput = document.getElementById('message-input');
-    const message = messageInput.value.trim();
-    if (message !== '') {
-      const chatMessages = document.querySelector('.chat-messages');
-      const messageElement = document.createElement('div');
-      messageElement.classList.add('message');
-      messageElement.textContent = message;
-      chatMessages.appendChild(messageElement);
-      messageInput.value = '';
-      chatMessages.scrollTop = chatMessages.scrollHeight;
+// Add any JavaScript functionality here
+
+// Example: Display a confirmation message when adding a product to the cart
+const addToCartButtons = document.querySelectorAll('.btn');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const productName = button.parentNode.querySelector('h3').textContent;
+        alert(`Added ${productName} to the cart!`);
+    });
+});
+
+// Example: Implement a simple image slider for the hero section
+const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+let currentIndex = 0;
+const heroImage = document.querySelector('#hero img');
+
+function changeImage() {
+    heroImage.src = images[currentIndex];
+}
+
+function nextImage() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
     }
-  }
-  
-  // Event listener
-  document.getElementById('send-button').addEventListener('click', sendMessage);
-  document.getElementById('message-input').addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      sendMessage();
+    changeImage();
+}
+
+function previousImage() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
     }
-  });
+    changeImage();
+}
+
+// Example: Automatically switch images every 3 seconds
+setInterval(nextImage, 3000);
